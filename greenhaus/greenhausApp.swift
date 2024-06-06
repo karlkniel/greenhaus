bfcf4f29f85c512b46b1840c3d959b270492d98f
kni3l <karlkniel@gmail.com>
2024-06-06 15:13:38 -0400

    Initial commit

---
+++ greenhaus/greenhausApp.swift
@@ -0,0 +1,17 @@
+//
+//  greenhausApp.swift
+//  greenhaus
+//
+//  Created by Karl Kniel on 5/31/24.
+//
+
+import SwiftUI
+
+@main
+struct greenhausApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
